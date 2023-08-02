ce82296c33b8f5814b376923065144afb3bcf15e
syed0113 <141040155+syed0113@users.noreply.github.com>
2023-08-02 13:00:51 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,18 @@
+class Addition
+{
+	public static void main(String args[])
+	{
+		int number1,number2,res;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter num1 value");
+		number1=s.nextInt();
+		System.out.println("Enter num2 vlue");
+		number2=s.nextInt();
+		res=number1+number2;
+		System.out.println("The result is "+res);
+		res=numeber1*number2;
+		System.out.println("The result of product is : "+res);
+		res=numeber1-number2;
+		System.out.println("The result of subtraction is : "+res);		
+	}
+}
